87d4f85b5a2bbf721152b36e16e733c99b7025c6
chuqol <151944573+chuqol@users.noreply.github.com>
2023-12-29 19:25:19 +0100

    Add files via upload

---
+++ evolution_instantnoodlep.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#               2022-2023 The Evolution X Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from instantnoodlep device
+$(call inherit-product, device/oneplus/instantnoodlep/device.mk)
+
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+
+EXTRA_UDFPS_ANIMATIONS := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+
+PRODUCT_NAME := evolution_instantnoodlep
+PRODUCT_DEVICE := instantnoodlep
+PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := IN2025
+
+PRODUCT_SYSTEM_NAME := OnePlus8Pro
+PRODUCT_SYSTEM_DEVICE := OnePlus8Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="OnePlus8Pro-user 13 RKQ1.211119.001 Q.129a9e4-1-2 release-keys" \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+iBUILD_FINGERPRINT := OnePlus/OnePlus8Pro/OnePlus8Pro:13/RKQ1.211119.001/Q.129a9e4-1-2:user/release-keys
